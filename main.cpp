6716b3ef6639ffa25dd5706b61baedb23035c088
Luka COUTANT <luka.coutant@etu.univ-lyon1.fr>
2024-01-26 12:37:31 +0100

    ajout ending main

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@
 
 int main()
 {
-    int sizex, sizey, startingx, startingy;
+    int sizex, sizey, startingx, startingy, endingx, endingy;
     std::cout << "Enter the size of the maze (x y): ";
     std::cin >> sizex >> sizey;
 
     std::cout << "Enter starting position (x y): ";
     std::cin >> startingx >> startingy;
 
+    std::cout << "Enter ending position (x y): ";
+    std::cin >> endingx >> endingy;
+
     Maze maze(sizex, sizey);
     maze[startingy - 1][startingx - 1] = 'S';
-
+    maze[endingy - 1][endingx - 1] = 'E';
+    
     maze.display();
 
     return 0;
